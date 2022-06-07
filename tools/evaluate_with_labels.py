6fedd123d50c3379fdbc6d809c7a1880b26682fc
eurc17 <50904241+eurc17@users.noreply.github.com>
2022-06-07 19:32:27 +0800

    Added an evaluation script

---
+++ tools/evaluate_with_labels.py
@@ -0,0 +1,89 @@
+import pcdet.datasets.kitti.kitti_object_eval_python.kitti_common as kitti
+from pcdet.datasets.kitti.kitti_object_eval_python.eval import (
+    get_coco_eval_result,
+    get_official_eval_result,
+)
+import argparse
+import os
+from pcdet.utils import calibration_kitti
+from pathlib import Path
+
+
+def get_calib(calib_file):
+    assert calib_file.exists()
+    return calibration_kitti.Calibration(calib_file)
+
+
+def _read_imageset_file(path):
+    with open(path, "r") as f:
+        lines = f.readlines()
+    return [int(line) for line in lines]
+
+
+def evaluate(
+    label_path,
+    result_path,
+    calib,
+    current_class=0,
+    coco=False,
+    score_thresh=-1,
+):
+    dt_annos = kitti.get_label_annos(result_path)
+    if score_thresh > 0:
+        dt_annos = kitti.filter_annos_low_score(dt_annos, score_thresh)
+    gt_annos = kitti.get_label_annos(label_path)
+    if coco:
+        return get_coco_eval_result(gt_annos, dt_annos, current_class, calib)
+    else:
+        return get_official_eval_result(gt_annos, dt_annos, current_class, calib)
+
+
+def main(args):
+    calib = get_calib(Path(args.calib_file_path))
+    result, result_dict = evaluate(
+        args.gt_label_dir, args.predict_label_dir, calib, coco=False
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="convert vector of bboxpvrcnn to kitti labels",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "-p",
+        "--predict_label_dir",
+        type=str,
+        required=True,
+        help="the path to the directory storing prediction labels",
+    )
+    parser.add_argument(
+        "-g",
+        "--gt_label_dir",
+        type=str,
+        required=True,
+        help="the path to the directory storing gt labels",
+    )
+    parser.add_argument(
+        "-c",
+        "--calib_file_path",
+        type=str,
+        required=True,
+        help="Path to the calibration file",
+    )
+
+    args = parser.parse_args()
+    if not os.path.exists(args.predict_label_dir):
+        print("predict_label_dir doesn't exists!")
+
+    if not os.path.isdir(args.predict_label_dir):
+        print("predict_label_dir is not a directory!")
+
+    if not os.path.exists(args.gt_label_dir):
+        print("gt_label_dir doesn't exists!")
+
+    if not os.path.isdir(args.gt_label_dir):
+        print("gt_label_dir is not a directory!")
+
+    main(args)
